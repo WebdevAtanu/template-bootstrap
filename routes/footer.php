6b7ebe89d69045a0a987e1c1f92ea9bfb314504d
Atanu <atanum991@gmail.com>
2024-03-13 22:07:10 +0530

    mod

--- routes/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="fixed-bottom bg-black p-1" id="footer">
-
-	
-	<p class="m-0 text-center text-light">Copyright &copy; 2024. Designed and Developed by <span><a href="" class="text-decoration-none text-danger">Atanu</a></span></p>
-</div>
